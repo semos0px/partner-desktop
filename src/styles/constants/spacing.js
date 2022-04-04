1b03ba90ed0c4f3c367abd4b660330adfeebdddb
semos0px <semos0px@gmail.com>
2022-04-04 12:48:21 +0900

    📝 Add constants about spacing

---
+++ src/styles/constants/spacing.js
@@ -0,0 +1,15 @@
+const spacing = {
+  margin: {
+    tiny: 10,
+    small: 30,
+    base: 50,
+    large: 100,
+  },
+  padding: {
+    small: 10,
+    base: 20,
+    large: 50,
+  },
+};
+
+export default spacing;
