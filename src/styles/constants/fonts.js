df64979201eb39f9c73ca7a3003339bada9f5ae4
semos0px <semos0px@gmail.com>
2022-04-04 12:48:08 +0900

    📝 Add constants about font

---
+++ src/styles/constants/fonts.js
@@ -0,0 +1,12 @@
+const fonts = {
+  kr: {
+    primary: "Noto Sans KR",
+    secondary: {
+      regular: "NanumSquare",
+      bold: "NanumSquareBold",
+    },
+    point: "dangdanghae",
+  },
+};
+
+export default fonts;
