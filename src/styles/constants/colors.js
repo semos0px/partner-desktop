7f0af7e937a1b2fb891068ce8113d8000477f045
semos0px <semos0px@gmail.com>
2022-04-04 12:47:58 +0900

    📝 Add constants about color

---
+++ src/styles/constants/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  lightBlue: "#EEF2FD",
+  blue: "#2C84EE",
+  yellow: "#FEE500",
+  lightRed: "#E635521A",
+  red: "#E63553",
+  violet: "#6D37BE",
+  lightGray: "#F9F9F9",
+  darkGray: "#707070",
+  gray: "#ECECEC",
+  vanilla: "#F4F4F4",
+  white: "#fff",
+  black: "#000",
+};
+
+export default colors;
