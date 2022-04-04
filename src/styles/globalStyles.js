055bfbbf4abbebe7db461ac759459ba842b53401
semos0px <semos0px@gmail.com>
2022-04-04 12:43:19 +0900

    ✨ Add globalStyles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,73 @@
+import { createGlobalStyle } from "styled-components";
+import normalize from "styled-normalize";
+import colors from "./constants/colors";
+import fonts from "./constants/fonts";
+
+const GlobalStyles = createGlobalStyle`
+    ${normalize};
+
+    @font-face {
+      font-family: "dangdanghae";
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.2/Cafe24Dangdanghae.woff') format('woff');
+    }
+    
+    /* Additional Customizing reset CSS */
+    * {
+        margin: 0;
+        font-family: ${fonts.kr.primary};
+        box-sizing: border-box;
+    }
+
+    html {
+        font-family: ${fonts.kr.primary};
+        font-size: 16px;
+    }
+
+    body {
+        font-family: ${fonts.kr.primary};
+        color: ${colors.black};
+        background-color: ${colors.white};
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    button,
+    input,
+    select,
+    textarea {
+        background-color: transparent;
+        border: 0;
+    }
+
+    button:focus,
+    input:focus,
+    select:focus,
+    textarea:focus {
+        outline: none;
+        box-shadow: none;
+    }
+
+    a,
+    button {
+        cursor: pointer;
+    }
+    
+    button {
+        padding: 0;
+    }
+
+    ul,
+    ol {
+        padding-left: 0;
+        list-style: none;
+    }
+`;
+
+export default GlobalStyles;
