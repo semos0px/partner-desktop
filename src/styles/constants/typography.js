371a41e0212efd4fc61d891b9b51ee9569f6ce65
semos0px <semos0px@gmail.com>
2022-04-04 12:48:41 +0900

    📝 Add constants about typography

---
+++ src/styles/constants/typography.js
@@ -0,0 +1,17 @@
+const typography = {
+  weight: {
+    thin: 400,
+    regular: 500,
+    bold: 700,
+  },
+  size: {
+    micro: 12,
+    tiny: 14,
+    small: 16,
+    base: 18,
+    medium: 20,
+    large: 26,
+  },
+};
+
+export default typography;
