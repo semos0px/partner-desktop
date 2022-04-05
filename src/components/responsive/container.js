39b36d3a9ddd74a3da477a7b087b900cf784494b
semos0px <semos0px@gmail.com>
2022-04-05 19:05:21 +0900

    🐛 Fix a bug about position property

--- src/components/responsive/container.js
+++ src/components/responsive/container.js
@@ -2,14 +2,16 @@ import styled from "styled-components";
 import responsive from "../../styles/constants/responsive";
 
 const SContainer = styled.div`
+  position: ${({ isRelative }) => isRelative && `relative`};
   width: ${responsive.maxWidth.sm}px;
   min-height: 100vh;
   margin: 0 auto;
   padding: 0 ${responsive.margin.sm}px;
+  background-color: pink;
 `;
 
-const Container = ({ children }) => {
-  return <SContainer>{children}</SContainer>;
+const Container = ({ children, isRelative = false }) => {
+  return <SContainer isRelative={isRelative}>{children}</SContainer>;
 };
 
 export default Container;
