6227a08b6c237dab3d2accfaff3c4c36ac53d72a
semos0px <semos0px@gmail.com>
2022-04-05 18:51:43 +0900

    ✨ Create NotFoundPage

---
+++ src/pages/notFound.js
@@ -0,0 +1,11 @@
+import PageLayout from "../layouts/pageLayout";
+
+const NotFound = () => {
+  return (
+    <PageLayout>
+      <p>Not Found</p>
+    </PageLayout>
+  );
+};
+
+export default NotFound;
