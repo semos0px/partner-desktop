860a0d545fea8307d0732fced2fea2601f248c8d
semos0px <semos0px@gmail.com>
2022-04-05 18:55:46 +0900

    🚧 Change absolute to fixed

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import chevron from "../assets/icon/page/chevron.svg";
+import base from "../styles/constants/base";
+import colors from "../styles/constants/colors";
+import typography from "../styles/constants/typography";
 import flexbox from "../styles/func/flexbox";
 
 const SHeader = styled.header`
   width: 100%;
-  height: 52px;
+  height: ${base.headerHeight}px;
   ${flexbox()};
-  background-color: coral;
-  position: relative;
+  position: fixed;
+  top: 0;
+  left: 0;
+  font-weight: ${typography.weight.regular};
+  background-color: ${colors.white};
 `;
 
 const GoBackButton = styled.button`
   position: absolute;
-  left: -10px;
+  left: 10px;
   padding: 10px;
 
   img {
@@ -29,13 +35,11 @@ const Header = ({ title, isGoBack = false }) => {
   return (
     <SHeader isGoBack={isGoBack}>
       {isGoBack && (
-        <>
-          <GoBackButton onClick={goBackHandler}>
-            <img src={chevron} alt="뒤로가기" />
-          </GoBackButton>
-          {title}
-        </>
+        <GoBackButton onClick={goBackHandler}>
+          <img src={chevron} alt="뒤로가기" />
+        </GoBackButton>
       )}
+      {title}
     </SHeader>
   );
 };
