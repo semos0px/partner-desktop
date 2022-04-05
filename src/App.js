ce699c5d80e2d6023b06381be4f916bd1b0feee3
semos0px <semos0px@gmail.com>
2022-04-05 18:51:24 +0900

    🚧 Implement routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import GNB from "./layouts/gnb";
+import GlobalLayout from "./layouts/globalLayout";
+import SignInPage from "./pages/signIn";
+import SignUpPage from "./pages/signIn/signup";
+import ProfilePage from "./pages/profile";
+import ClassPage from "./pages/class";
+import InquiryPage from "./pages/inquiry";
+import SalesPage from "./pages/sales";
+import ViewMore from "./pages/viewMore";
+import NotFound from "./pages/notFound";
+import ClassDetailPage from "./pages/class/detail";
+import AuthProvider, { ProtectRouter } from "./context/auth";
+import EditUserInfoPage from "./pages/user/edit";
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AuthProvider>
+        <GlobalLayout>
+          <BrowserRouter>
+            <ProtectRouter>
+              <Routes>
+                <Route index element={<SignInPage />} />
+                <Route path="signup" element={<SignUpPage />} />
+
+                <Route path=":pid" element={<EditUserInfoPage />} />
+
+                <Route path="profile" element={<ProfilePage />} />
+
+                <Route path="class/*" element={<ClassPage />}>
+                  <Route path=":cid" element={<ClassDetailPage />} />
+                </Route>
+
+                <Route path="inquiry" element={<InquiryPage />} />
+                <Route path="sales" element={<SalesPage />} />
+                <Route path="viewmore" element={<ViewMore />} />
+                <Route path="signup" element={<SignUpPage />} />
+
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </ProtectRouter>
+
+            <GNB />
+          </BrowserRouter>
+        </GlobalLayout>
+      </AuthProvider>
     </div>
   );
 };
