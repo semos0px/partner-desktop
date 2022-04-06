29524182f4634f808dd4823bd81c70cb74e327b5
semos0px <semos0px@gmail.com>
2022-04-06 15:47:58 +0900

    ✨ Finish loginBox component

--- src/components/loginBox.js
+++ src/components/loginBox.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 import base from "../styles/constants/base";
 import colors from "../styles/constants/colors";
 import fonts from "../styles/constants/fonts";
+import responsive from "../styles/constants/responsive";
 import typography from "../styles/constants/typography";
 import flexbox from "../styles/func/flexbox";
+import transition from "../styles/func/transition";
 
 const Box = styled.div`
   width: 100%;
@@ -24,11 +26,64 @@ const Title = styled.p`
   margin-bottom: 10px;
 `;
 
+const ButtonBox = styled.div`
+  width: 100%;
+  margin-top: 50px;
+  padding: 0 40px;
+
+  button {
+    width: 100%;
+    height: 66px;
+    padding: 10px 20px;
+    border-radius: 40px;
+    transition: ${transition("opacity")};
+    font-size: ${typography.size.medium}px;
+    font-weight: ${typography.weight.regular};
+
+    &:first-child {
+      margin-bottom: 10px;
+    }
+  }
+
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+
+  ${responsive.mediaQuery.mobile} {
+    display: flex;
+
+    button {
+      margin: 0 10px;
+    }
+  }
+`;
+
+const KakaoButton = styled.button`
+  background-color: ${colors.yellow};
+`;
+
+const AppleButton = styled.button`
+  background-color: ${colors.black};
+  color: ${colors.white};
+`;
+
 const LoginBox = () => {
+  const loginHandler = () => {};
+
   return (
     <Box>
       <Title>login</Title>
       <p>카카오톡/애플로 회원가입부터 로그인까지 한번에 해결!</p>
+
+      <ButtonBox>
+        <KakaoButton type="button" onClick={loginHandler}>
+          카카오톡으로 로그인하기
+        </KakaoButton>
+        <AppleButton type="button" onClick={loginHandler}>
+          Apple로 로그인하기
+        </AppleButton>
+      </ButtonBox>
     </Box>
   );
 };
