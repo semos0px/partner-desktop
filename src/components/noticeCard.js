02af8b2c3578cc5108b54d34c60b0f6f4662d02b
Hwajiin <borderline0px@gmail.com>
2022-04-07 08:06:21 +0900

    ✨ Notice Card component 구현

---
+++ src/components/noticeCard.js
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+import base from "../styles/constants/base";
+import colors from "../styles/constants/colors";
+import responsive from "../styles/constants/responsive";
+import typography from "../styles/constants/typography";
+
+const Card = styled.li`
+  width: 100%;
+  height: 250px;
+  border-radius: ${base.borderRadius}px;
+  background-color: ${colors.white};
+  box-shadow: ${base.boxShadow};
+  overflow: hidden;
+  margin: 10px;
+
+  ${responsive.mediaQuery.mobile} {
+    width: calc(50% - 20px);
+  }
+`;
+
+const A = styled.a`
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
+
+const ImgBox = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: ${colors.lightGray};
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(transparent 1%, ${colors.darkGray});
+  }
+`;
+
+const MetaData = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Date = styled.span`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: ${colors.white};
+  background-color: rgba(0, 0, 0, 0.2);
+  padding: 5px;
+  border-radius: 5px;
+  font-size: ${typography.size.small}px;
+`;
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+`;
+
+const NoticeIcon = styled.span`
+  display: inline-block;
+  background-color: ${colors.red};
+  color: ${colors.white};
+  padding: 5px;
+  border-radius: 5px;
+  font-weight: ${typography.weight.regular};
+  font-size: ${typography.size.small}px;
+  margin-bottom: 5px;
+`;
+
+const Title = styled.p`
+  font-size: ${typography.size.medium}px;
+  color: ${colors.white};
+`;
+
+const NoticeCard = ({ notice }) => {
+  return (
+    <Card>
+      <A href={notice.url}>
+        <ImgBox />
+
+        <MetaData>
+          <Date>{`${notice.year} - ${notice.month}/${notice.day}`}</Date>
+
+          <Footer>
+            <NoticeIcon>공지사항</NoticeIcon>
+            <Title>{notice.title}</Title>
+          </Footer>
+        </MetaData>
+      </A>
+    </Card>
+  );
+};
+
+export default NoticeCard;
