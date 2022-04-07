a2155190d501dd0766a3131d0afb4cdca44a6f72
semos0px <semos0px@gmail.com>
2022-04-07 18:56:07 +0900

    📝 Add mock up for profile detail data

---
+++ src/data/profile/detail.js
@@ -0,0 +1,19 @@
+import lessonImg from "../../assets/images/profile/thumbnail.png";
+
+const profileDetailData = {
+  id: "123",
+  category: {
+    main: "워터스포츠",
+    sub: "프리다이빙",
+  },
+  title: "핸강사(김현주)",
+  region: ["서울 송파/강동", "경기 수원"],
+  image: {
+    main: "",
+    background: [],
+    lesson: [lessonImg, lessonImg, lessonImg, lessonImg, lessonImg, lessonImg],
+  },
+  career: ["SDI 스쿠버다이빙 강사", "SDI 스쿠버다이빙 강사", "수중 사진 작가"],
+};
+
+export default profileDetailData;
