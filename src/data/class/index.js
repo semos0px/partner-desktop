c10c6478e57b9eb7090d22636c0e714d83586cd6
semos0px <semos0px@gmail.com>
2022-04-07 16:59:59 +0900

    📝 Add data for class

---
+++ src/data/class/index.js
@@ -0,0 +1,26 @@
+const classList = {
+  general: [
+    {
+      title: "체험 다이빙",
+      status: "확인 예약",
+      price: 50000,
+      thumbnail: "",
+    },
+    {
+      title: "베이직 과정 Lv.1(입문 자격증반)",
+      status: "확정 예약",
+      price: 140000,
+      thumbnail: "",
+    },
+  ],
+  crew: [
+    {
+      title: "프리다이빙 입문 1DAY 레슨, 초보자들 여기로...",
+      status: "확인 예약",
+      price: 40000,
+      thumbnail: "",
+    },
+  ],
+};
+
+export default classList;
