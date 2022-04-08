23372dbb5b04d61bdd8c82cbeb97231f2a17e079
Hwajiin <borderline0px@gmail.com>
2022-04-08 09:01:11 +0900

    📝 Add list for reports data

---
+++ src/data/reports/index.js
@@ -0,0 +1,29 @@
+const reportsList = [
+  {
+    id: 0,
+    nickname: "따라따라따따",
+    lesson: "입문 레벨(1DAY 과정)",
+    category: "프리다이빙",
+    status: false,
+    datetime: "2022-01-22",
+  },
+  {
+    id: 1,
+    nickname: "기욤이",
+    lesson: "체험 다이빙",
+    category: "프리다이빙",
+    status: false,
+    datetime: "2022-01-22",
+  },
+
+  {
+    id: 2,
+    nickname: "차쿠차구",
+    lesson: "강사 과정",
+    category: "프리다이빙",
+    status: true,
+    datetime: "2022-01-22",
+  },
+];
+
+export default reportsList;
