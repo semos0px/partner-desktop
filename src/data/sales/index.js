77be1d7e8bb065cb2fadda31718e185a3ec30305
semos0px <semos0px@gmail.com>
2022-04-08 19:20:47 +0900

    📝 Write data about sales

---
+++ src/data/sales/index.js
@@ -0,0 +1,40 @@
+const salesData = {
+  income: 2039800,
+
+  uncertain: [
+    {
+      id: 0,
+      category: "스쿠버다이빙",
+      title: "오픈워터 코스",
+      price: 360000,
+      datetime: "2월 13일(일)",
+    },
+    {
+      id: 1,
+      category: "스쿠버다이빙",
+      title: "오픈워터 코스",
+      price: 360000,
+      datetime: "2월 17일(일)",
+    },
+  ],
+  certain: [
+    {
+      id: 0,
+      category: "스쿠버다이빙",
+      title: "오픈워터 코스",
+      price: 360000,
+      status: true,
+      datetime: "2월 13일(일)",
+    },
+    {
+      id: 1,
+      category: "스쿠버다이빙",
+      title: "오픈워터 코스",
+      price: 360000,
+      status: false,
+      datetime: "2월 13일(일)",
+    },
+  ],
+};
+
+export default salesData;
