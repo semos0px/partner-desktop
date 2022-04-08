f35b66d3b2e875f6c1badb69b239c8d324d3b44f
semos0px <semos0px@gmail.com>
2022-04-08 19:20:21 +0900

    💄 Add icon for search

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,12 +1,30 @@
 import InputField from "./inputField";
+import searchIcon from "../assets/icon/input/search.svg";
+import styled from "styled-components";
+import base from "../styles/constants/base";
+
+const Box = styled.div`
+  position: relative;
+  width: 100%;
+
+  img {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
+`;
 
 const SearchBar = ({ value, changeHandler, placeholder = "" }) => {
   return (
-    <InputField
-      placeholder={placeholder}
-      value={value}
-      changeHandler={changeHandler}
-    />
+    <Box>
+      <InputField
+        placeholder={placeholder}
+        value={value}
+        changeHandler={changeHandler}
+        isIcon={true}
+      />
+      <img src={searchIcon} />
+    </Box>
   );
 };
 
