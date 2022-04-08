20e86790f63155616a0672be3bbbc5cb46c4543f
semos0px <semos0px@gmail.com>
2022-04-08 19:21:38 +0900

    📝 Write data about reports detail

---
+++ src/data/reports/detail.js
@@ -0,0 +1,23 @@
+const reportsDetailData = [
+  {
+    id: 0,
+    nickname: "따라따라따따님",
+    lesson: "입문 레벨1(1DAY 과정)",
+    category: "프리다이빙",
+    date: "2021년 12월 29일[1일차]",
+    report: [
+      {
+        progressRate: 50,
+        content:
+          "물과 친해지는 과정으로 물 속에서의 기본 호흡법과 장비 사용법에 대해서 알아봤어요~",
+        feedback: {
+          me: "물을 많이 무서워하지만 계속 연습하면 충분히 하실 수 있을 것 같네욧 ^^ 앞으로도 포기하지말고 도전해 보세요~",
+          student:
+            "물이 차가워서 더 놀랐던 것 같아요.. 그런점이 보완된다면 더 체험하고 싶고 다음에는 친구랑 같이 해보고 싶네요!!",
+        },
+      },
+    ],
+  },
+];
+
+export default reportsDetailData;
