eecceae4e2bb00ef501787caf7150403bafb91df
semos0px <semos0px@gmail.com>
2022-04-08 19:21:08 +0900

    📝 Write data about inquiry

---
+++ src/data/inquiry/index.js
@@ -0,0 +1,29 @@
+const inquiryList = [
+  {
+    id: 0,
+    nickname: "마늘쫑이야",
+    lesson: "입문 레벨(1DAY 과정)",
+    status: false,
+    datetime: "2022-01-22",
+    comment: "스쿠버다이빙은 처음 배워보는데 수영을 못해도 가능할...",
+  },
+  {
+    id: 1,
+    nickname: "기욤이",
+    lesson: "공보경",
+    status: false,
+    datetime: "2022-01-22",
+    comment: "스쿠버다이빙은 처음 배워보는데 수영을 못해도 가능할...",
+  },
+
+  {
+    id: 2,
+    nickname: "차쿠차구",
+    lesson: "체험 다이빙",
+    status: true,
+    datetime: "2022-01-22",
+    comment: "체험 다이빙을 한 뒤에 레벨 원 과정을 하면 할인이 들...",
+  },
+];
+
+export default inquiryList;
