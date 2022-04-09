7bb2f2226eb6af3fd94e84275932e83fd171b4cf
Hwajiin <borderline0px@gmail.com>
2022-04-10 01:29:25 +0900

    💄 Add icon and style when to active

--- src/layouts/gnb.js
+++ src/layouts/gnb.js
@@ -1,52 +1,76 @@
-import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import NavList from "../components/navList";
-import profileIcon from "../assets/icon/navbar/profile.svg";
 import colors from "../styles/constants/colors";
 import base from "../styles/constants/base";
 import responsive from "../styles/constants/responsive";
+import viewmoreIcon from "../assets/icon/gnb/viewmore.png";
+import viewmoreActiveIcon from "../assets/icon/gnb/viewmore-a.png";
+import profileIcon from "../assets/icon/gnb/profile.png";
+import profileActiveIcon from "../assets/icon/gnb/profile-a.png";
+import salesIcon from "../assets/icon/gnb/sales.png";
+import salesActiveIcon from "../assets/icon/gnb/sales-a.png";
+import classIcon from "../assets/icon/gnb/class.png";
+import classActiveIcon from "../assets/icon/gnb/class-a.png";
+import inquiryIcon from "../assets/icon/gnb/inquiry.png";
+import inquiryActiveIcon from "../assets/icon/gnb/inquiry-a.png";
+import zIndex from "../styles/constants/z-index";
 
 const Navbar = styled.nav`
   width: 100%;
   max-width: ${responsive.maxWidth.sm}px;
   position: fixed;
   bottom: 0;
+  padding: 0 10px;
 
   height: ${base.height.gnb}px;
   border-top-left-radius: ${base.borderRadius}px;
   border-top-right-radius: ${base.borderRadius}px;
   box-shadow: ${base.boxShadow};
   background-color: ${colors.white};
+  z-index: ${zIndex.navbar};
 `;
 
 const GNB = () => {
-  const { pathname } = useLocation();
-
   const navList = [
     {
       text: "프로필",
       pathname: "profile",
-      icon: profileIcon,
+      icon: {
+        default: profileIcon,
+        active: profileActiveIcon,
+      },
     },
     {
       text: "강습",
       pathname: "class",
-      // icon:
+      icon: {
+        default: classIcon,
+        active: classActiveIcon,
+      },
     },
     {
       text: "문의",
       pathname: "inquiry",
-      // icon:
+      icon: {
+        default: inquiryIcon,
+        active: inquiryActiveIcon,
+      },
     },
     {
       text: "판매",
       pathname: "sales",
-      // icon:
+      icon: {
+        default: salesIcon,
+        active: salesActiveIcon,
+      },
     },
     {
       text: "더보기",
       pathname: "viewmore",
-      // icon:
+      icon: {
+        default: viewmoreIcon,
+        active: viewmoreActiveIcon,
+      },
     },
   ];
 
