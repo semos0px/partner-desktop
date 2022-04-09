ed30009b11c2c8a1653494b99bd45f4798c5492e
Hwajiin <borderline0px@gmail.com>
2022-04-10 01:32:41 +0900

    📝 Add data about sales detail

---
+++ src/data/sales/detail.js
@@ -0,0 +1,23 @@
+const salesDetailData = {
+  id: 0,
+  status: false,
+  partner: "고크리스틴",
+  category: "스쿠버다이빙",
+  title: "체험 다이빙과정",
+  member: {
+    mobile: "#",
+    nickname: "고크리스틴",
+  },
+  payment: {
+    number: 33409,
+    date: "2022-02-06",
+    time: "12:00 - 15:00",
+    price: {
+      product: 78000,
+      option: 20000,
+      cnt: 50000,
+    },
+  },
+};
+
+export default salesDetailData;
