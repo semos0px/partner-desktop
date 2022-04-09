0207fea24740d63174e375c8c45dd3dbbbc5c4e1
Hwajiin <borderline0px@gmail.com>
2022-04-10 01:31:52 +0900

    🚧 Edit default style of range field

--- src/components/rangeField.js
+++ src/components/rangeField.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+import base from "../styles/constants/base";
 import colors from "../styles/constants/colors";
+import typography from "../styles/constants/typography";
 
 const Box = styled.div`
   margin-bottom: 20px;
   display: flex;
+  position: relative;
 `;
 
 const Label = styled.label`
@@ -12,30 +15,50 @@ const Label = styled.label`
 `;
 
 const Input = styled.input`
-  -webkit-appearance: none;
-  /* all: unset; */
   width: 100%;
-  height: 30px;
+  -webkit-appearance: none;
+  height: 20px;
   background-color: ${colors.vanilla};
   border-radius: 30px;
+  background: ${({ value }) =>
+    `linear-gradient(to right, ${colors.blue} 0%, ${colors.blue} ${value}%, ${colors.vanilla} ${value}%, ${colors.vanilla} 100%)`};
 
   &::-webkit-slider-thumb {
-    position: relative;
-    height: 40px;
-    width: 40px;
+    -webkit-appearance: none;
+    height: 25px;
+    width: 25px;
     background: ${colors.blue};
     border-radius: 50%;
     cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -5px;
+    margin-top: 0px;
+    box-shadow: ${base.boxShadow};
   }
 
-  &::-webkit-slider-runnable-track {
-    height: 100%;
-    background-color: ${colors.blue};
-    border-radius: 40px;
-    -webkit-appearance: none;
+  &::-webkit-track {
   }
+
+  /** FF*/
+  &::-moz-range-progress {
+    background-color: ${colors.blue};
+  }
+  &::-moz-range-track {
+    background-color: ${colors.blue};
+  }
+  /* IE*/
+  &::-ms-fill-lower {
+    background-color: ${colors.blue};
+  }
+  &::-ms-fill-upper {
+    background-color: ${colors.blue};
+  }
+`;
+
+const Text = styled.span`
+  position: absolute;
+  left: ${({ value }) => `calc(110px + ${value}px)`};
+  top: 2px;
+  color: ${colors.white};
+  font-size: ${typography.size.tiny}px;
 `;
 
 const RangeField = ({ label, name, value, inputHandler }) => {
@@ -47,10 +70,12 @@ const RangeField = ({ label, name, value, inputHandler }) => {
         type="range"
         min="0"
         max="100"
+        step="5"
         value={value}
         name={name}
         onInput={inputHandler}
       />
+      {value >= 5 && <Text value={value}>{value}%</Text>}
     </Box>
   );
 };
