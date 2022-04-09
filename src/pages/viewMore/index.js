f7ed2553f735b622e8cf5e1f8ce5aa1c07fdacbc
Hwajiin <borderline0px@gmail.com>
2022-04-10 01:38:06 +0900

    ✨ 더보기 페이지 구현하기

--- src/pages/viewMore/index.js
+++ src/pages/viewMore/index.js
@@ -5,21 +5,132 @@ import PageLayout from "../../layouts/pageLayout";
 import base from "../../styles/constants/base";
 import colors from "../../styles/constants/colors";
 import flexbox from "../../styles/func/flexbox";
-import editIcon from "../../assets/icon/page/edit.svg";
+import editIcon from "../../assets/icon/viewmore/edit.svg";
+import RowLayout from "../../layouts/rowLayout";
+import typography from "../../styles/constants/typography";
+import fonts from "../../styles/constants/fonts";
+import chevron from "../../assets/icon/page/chevron.svg";
+import reportsIcon from "../../assets/icon/viewmore/reports.png";
+import reviewIcon from "../../assets/icon/viewmore/review.png";
+import kakaoIcon from "../../assets/icon/login/kakao.svg";
+import responsive from "../../styles/constants/responsive";
 
 const Section = styled.section`
-  padding: 0px 50px;
+  padding: 0px 20px;
   border-radius: ${base.borderRadius}px;
   box-shadow: ${base.boxShadow};
+  background-color: ${colors.white};
+  font-family: ${fonts.kr.secondary};
+
+  &:not(:last-child) {
+    margin-bottom: 50px;
+  }
+
+  ${responsive.mediaQuery.mobile} {
+    padding: 0px 50px;
+  }
 `;
 
-const SLink = styled(Link)``;
+const KakaoSection = styled.section`
+  padding: 0px 20px;
+  border-radius: ${base.borderRadius}px;
+  box-shadow: ${base.boxShadow};
+  background-color: ${colors.yellow};
+  font-family: ${fonts.kr.secondary};
+  margin-bottom: 20px;
+
+  ${responsive.mediaQuery.mobile} {
+    padding: 0px 50px;
+  }
+`;
+
+const FLink = styled(Link)``;
+
+const SLink = styled(Link)`
+  padding: 30px 0;
+  ${flexbox("space-between")};
+  width: 100%;
+
+  div {
+    ${flexbox("flex-start")}
+
+    img {
+      width: 20px;
+      height: 20px;
+      margin-right: 10px;
+    }
+  }
+`;
 
 const PartnerInfoBox = styled.div`
   ${flexbox("space-between")}
 `;
 
-const Data = styled.div``;
+const Data = styled.div`
+  padding: 30px 0;
+
+  p:not(:last-child) {
+    margin-bottom: 5px;
+  }
+
+  p:last-child {
+    font-size: ${typography.size.small}px;
+  }
+`;
+
+const Title = styled.p`
+  font-size: ${typography.size.medium}px;
+  margin-bottom: 20px;
+`;
+
+const Border = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: ${colors.vanilla};
+  border-radius: 5px;
+`;
+
+const Chevron = styled.img`
+  transform: rotate(-90deg);
+  width: 15px;
+  height: 15px;
+`;
+
+const ALink = styled.a`
+  padding: 30px 0;
+  ${flexbox("space-between")};
+  width: 100%;
+
+  div {
+    ${flexbox("flex-start")}
+
+    img {
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  padding: 30px 0;
+  ${flexbox("space-between")};
+  width: 100%;
+
+  div {
+    ${flexbox("flex-start")}
+
+    img {
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
+    }
+
+    span {
+      font-size: ${typography.size.small}px;
+    }
+  }
+`;
 
 const ViewMore = () => {
   // data fetching
@@ -29,22 +140,118 @@ const ViewMore = () => {
     mobile: "010-3305-3419",
   };
 
+  const kakaoInquiryHandler = () => {};
+
+  const logoutHandler = () => {
+    // TODO: auth context에서 logout 구현한거 가져올 것
+    // login 페이지로 redirect
+  };
+
+  const shareHandler = () => {};
+
   return (
     <PageLayout headerTitle="더보기" backgroundColor={colors.darkWhite}>
       <PaddingLayout>
-        <Section>
-          <PartnerInfoBox>
-            <Data>
-              <p>{partnerInfo.name}강사님</p>
-              <p>{partnerInfo.mobile}</p>
-            </Data>
-            <SLink to={`/${partnerInfo.id}`}>
-              <img src={editIcon} />
-            </SLink>
-          </PartnerInfoBox>
-        </Section>
+        <RowLayout>
+          <Section>
+            <PartnerInfoBox>
+              <Data>
+                <p>{partnerInfo.name}강사님</p>
+                <p>{partnerInfo.mobile}</p>
+              </Data>
+              <FLink to={`/${partnerInfo.id}`}>
+                <img src={editIcon} />
+              </FLink>
+            </PartnerInfoBox>
 
-        <Section></Section>
+            <Border />
+
+            <SLink to="/reports">
+              <div>
+                <img src={reportsIcon} />
+                <p>강습 리포트</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </SLink>
+
+            <Border />
+
+            <SLink to="/">
+              <div>
+                <img src={reviewIcon} />
+                <p>후기 답변 관리</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </SLink>
+          </Section>
+
+          <Title>고객센터</Title>
+
+          <KakaoSection>
+            <Button type="button" onClick={kakaoInquiryHandler}>
+              <div>
+                <img src={kakaoIcon} />
+                <p>
+                  카카오톡 문의 <span>(평일: 10:00 - 19:00)</span>
+                </p>
+              </div>
+
+              <Chevron src={chevron} />
+            </Button>
+          </KakaoSection>
+
+          <Section>
+            <SLink to="/faq">
+              <div>
+                <p>자주 묻는 질문</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </SLink>
+
+            <Border />
+
+            <SLink to="/notice">
+              <div>
+                <p>공지사항</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </SLink>
+
+            <Border />
+
+            <ALink href="http://reports.semos.kr/">
+              <div>
+                <p>리포츠 정보</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </ALink>
+
+            <Border />
+
+            <Button type="button" onClick={shareHandler}>
+              <div>
+                <p>세모스 공유하기</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </Button>
+
+            <Border />
+
+            <Button type="button" onClick={logoutHandler}>
+              <div>
+                <p>로그아웃</p>
+              </div>
+
+              <Chevron src={chevron} />
+            </Button>
+          </Section>
+        </RowLayout>
       </PaddingLayout>
     </PageLayout>
   );
