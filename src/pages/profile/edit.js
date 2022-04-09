b3b9ad9929c7476bfbf943811c92670c53e4f9f0
Hwajiin <borderline0px@gmail.com>
2022-04-10 01:41:40 +0900

    🚧 프로필 수정 페이지 구현 중

--- src/pages/profile/edit.js
+++ src/pages/profile/edit.js
@@ -1,7 +1,32 @@
+import styled from "styled-components";
+import PaddingLayout from "../../layouts/paddingLayout";
 import PageLayout from "../../layouts/pageLayout";
+import RowLayout from "../../layouts/rowLayout";
+import BottomButton from "../../modules/bottomButton";
+import colors from "../../styles/constants/colors";
+
+const Form = styled.form``;
 
 const ProfileEditPage = () => {
-  return <PageLayout>Profile Edit</PageLayout>;
+  const submitHandler = () => {
+    console.log("저장하기");
+  };
+
+  return (
+    <PageLayout headerTitle="프로필 수정" isGoBack={true}>
+      <PaddingLayout>
+        <RowLayout>
+          <Form onSubmit={submitHandler}></Form>
+
+          <BottomButton
+            text="저장하기"
+            clickHandler={submitHandler}
+            color={colors.blue}
+          />
+        </RowLayout>
+      </PaddingLayout>
+    </PageLayout>
+  );
 };
 
 export default ProfileEditPage;
