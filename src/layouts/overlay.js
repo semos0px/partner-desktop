b592e7ed80f14483b365f0ef6b6f39a6e69ac733
semos0px <semos0px@gmail.com>
2022-04-12 15:45:33 +0900

    💄 화면에 맞게 오버레이 레이아웃 수정하기

--- src/layouts/overlay.js
+++ src/layouts/overlay.js
@@ -1,19 +1,30 @@
 import styled from "styled-components";
+import responsive from "../styles/constants/responsive";
 import zIndex from "../styles/constants/z-index";
 
 const SOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  height: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.4);
-  backdrop-filter: blur(2px);
+  position: fixed;
+  top: 0;
+  left: 0;
+
   z-index: ${zIndex.overlay};
+
+  div {
+    max-width: ${responsive.maxWidth.sm}px;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    background-color: rgba(0, 0, 0, 0.4);
+    backdrop-filter: blur(2px);
+  }
 `;
 
 const Overlay = ({ toggleHandler }) => (
-  <SOverlay aria-hidden onClick={toggleHandler}></SOverlay>
+  <SOverlay aria-hidden onClick={toggleHandler}>
+    <div></div>
+  </SOverlay>
 );
 
 export default Overlay;
