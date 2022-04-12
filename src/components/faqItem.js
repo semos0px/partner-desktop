7c807fa6aaf9f12b033324e8602a851bbfbd3281
semos0px <semos0px@gmail.com>
2022-04-12 15:46:11 +0900

    💄 FAQ item 컴포넌트의 border-radius 적용하기

--- src/components/faqItem.js
+++ src/components/faqItem.js
@@ -4,6 +4,7 @@ import colors from "../styles/constants/colors";
 import flexbox from "../styles/func/flexbox";
 import chevronIcon from "../assets/icon/page/chevron.svg";
 import transition from "../styles/func/transition";
+import base from "../styles/constants/base";
 
 const Item = styled.li`
   width: 100%;
@@ -33,6 +34,7 @@ const AnswerBox = styled.div`
   ${flexbox()};
   padding: 0 20px;
   background-color: ${colors.lightBlue};
+  border-radius: ${base.borderRadius}px;
   max-height: ${({ open }) => (open ? "90px" : 0)};
   overflow: hidden;
   transition: ${transition("max-height")};
@@ -42,7 +44,13 @@ const AnswerBox = styled.div`
   }
 `;
 
-const FAQItem = ({ idx, faq }) => {
+const Border = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: ${colors.vanilla};
+`;
+
+const FAQItem = ({ idx, faq, isBorder = false }) => {
   const [open, setOpen] = useState(false);
 
   const openToggleHandler = () => {
@@ -50,16 +58,21 @@ const FAQItem = ({ idx, faq }) => {
   };
 
   return (
-    <Item onClick={openToggleHandler}>
-      <QuestionBox onClick={openToggleHandler} open={open}>
-        <p>{`${idx}. [${faq.category}] ${faq.question}`}</p>
-        <img src={chevronIcon} />
-      </QuestionBox>
+    <>
+      <Item onClick={openToggleHandler} isBorder={isBorder}>
+        <QuestionBox onClick={openToggleHandler} open={open}>
+          {faq.category && <p>{`${idx}. [${faq.category}] ${faq.question}`}</p>}
+          {!faq.category && <p>Q. {faq.question}</p>}
+          <img src={chevronIcon} />
+        </QuestionBox>
 
-      <AnswerBox open={open}>
-        <p>{faq.answer.replaceAll(/(<br>|<br \/>|<br\/>)/g, "\r\n")}</p>
-      </AnswerBox>
-    </Item>
+        <AnswerBox open={open}>
+          <p>{faq.answer.replaceAll(/(<br>|<br \/>|<br\/>)/g, "\r\n")}</p>
+        </AnswerBox>
+      </Item>
+
+      {isBorder && <Border />}
+    </>
   );
 };
 
