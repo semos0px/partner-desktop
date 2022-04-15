abf976b8f23481c5849149b7af170747b5f2269a
semos0px <semos0px@gmail.com>
2022-04-15 16:32:49 +0900

    📝 강습 일정 자료 작성하기

---
+++ src/data/calendar/index.js
@@ -0,0 +1,23 @@
+const calendarData = {
+  1: {
+    datetime: new Date(),
+    lesson: [
+      {
+        location: "서울 송파 잠실",
+        maximum: 1,
+        time: {
+          start: {
+            hour: "13",
+            minute: "00",
+          },
+          finish: {
+            hour: "14",
+            minute: "00",
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default calendarData;
