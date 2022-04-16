7a77f113d28c05a5f8e8944b9aaf11d7a5da0c1c
Hwajiin <borderline0px@gmail.com>
2022-04-16 17:12:59 +0900

    ✨ 위치 확인 모달 구현하기

--- src/components/classForm/locationInputField.js
+++ src/components/classForm/locationInputField.js
@@ -7,6 +7,27 @@ import flexbox from "../../styles/func/flexbox";
 import responsive from "../../styles/constants/responsive";
 import typography from "../../styles/constants/typography";
 import base from "../../styles/constants/base";
+import { useState } from "react";
+import OverlayPortal from "../../overlayPortal";
+import Overlay from "../../layouts/overlay";
+import Popup from "../../layouts/popup";
+
+const Modal = styled.div`
+  position: relative;
+  background-color: ${colors.white};
+  padding: 20px 40px;
+  border-radius: ${base.borderRadius}px;
+  text-align: center;
+  pointer-events: auto;
+
+  button {
+    position: absolute;
+    top: 15px;
+    right: 0px;
+    transform: translate(-50%, -50%);
+    padding: 5px;
+  }
+`;
 
 const Box = styled.div`
   width: 100%;
@@ -58,35 +79,60 @@ const LocationInputField = ({
   addHandler,
   deleteHandler,
 }) => {
+  const [open, setOpen] = useState(false);
+
+  const showDetailAddressHandler = () => {
+    setOpen(true);
+  };
+
   return (
-    <Box>
-      <MainAddress>
-        <button onClick={searchAddressHandler}>
-          <img src={regionIcon} />
-          <span>주소 검색</span>
-        </button>
+    <>
+      <Box>
+        <MainAddress>
+          <button onClick={searchAddressHandler}>
+            <img src={regionIcon} />
+            <span>주소 검색</span>
+          </button>
 
-        <p>인천 연수구 새말로 693-1</p>
-      </MainAddress>
+          <p>인천 연수구 새말로 693-1</p>
+        </MainAddress>
 
-      <SubAddress>
-        <input type="text" placeholder="상세주소 입력" name="location-sub" />
+        <SubAddress>
+          <input type="text" placeholder="상세주소 입력" name="location-sub" />
 
-        <AddButton addHandler={addHandler} />
-      </SubAddress>
+          <AddButton addHandler={addHandler} />
+        </SubAddress>
 
-      {/* 데이터 가져오기 */}
-      <ul>
-        <li>
-          <InfoTag
-            text="서울 송파 잠실"
-            color="blue"
-            isLine={true}
-            deleteHandler={deleteHandler}
-          />
-        </li>
-      </ul>
-    </Box>
+        {/* 데이터 가져오기 */}
+        <ul>
+          <li>
+            <InfoTag
+              text="서울 송파 잠실"
+              color="blue"
+              isLine={true}
+              deleteHandler={deleteHandler}
+              clickHandler={showDetailAddressHandler}
+              ispointer="true"
+            />
+          </li>
+        </ul>
+      </Box>
+
+      {open && (
+        <Popup>
+          <Modal>
+            <p>인천 연수구 송도문화로 123-1</p>
+            <p>송도풀장 203호</p>
+
+            <button onClick={() => setOpen(false)}>x</button>
+          </Modal>
+        </Popup>
+      )}
+
+      <OverlayPortal>
+        {open && <Overlay toggleHandler={() => setOpen(false)}></Overlay>}
+      </OverlayPortal>
+    </>
   );
 };
 
